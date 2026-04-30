a38b610485071610f8fe606818d2db3f295ea8e4
Gopi Vinay <gopivinay@Gopis-MacBook-Pro.local>
2026-05-01 01:40:58 +0530

    finds the mse value and the slope , i9ntercept using the gradient descent.

---
+++ scratch.md
@@ -0,0 +1,17 @@
+
+linear reggression => y = mx + b;
+MSE one value => (actualValue - predictedValue)^2;
+add all divided .
+  value + value + value ... / values count;
+
+  mGradeint => previous mgradient + error * cuurent value;
+  yGradeint => previous ygradient + error ;
+
+
+gradient => currentWeight - (learning rate - gradient);
+  
+
+
+guide propmt :
+
+i want build the neural network understand the gaph by ml. in the twitter there is video popular of the neral network video in which machine finds the graph pattern. so i want to build it. i will you my thought and process and check it. tell me if it is wrong . act as a guide.
---
+++ src/linear_regression.ts
@@ -0,0 +1,43 @@
+import { Data } from "./types.ts";
+
+const applyLinearRegression = (slope: number, x: number, intercept: number) => {
+  return slope * x + intercept;
+};
+
+function computeMSE(data : Data, slope : number, intercept : number) {
+  let error = 0;
+  for (let i = 0; i < data.length; i++) {
+    const yPred = slope * data[i].x + intercept;
+    error += Math.pow(yPred - data[i].actualX, 2);
+  }
+  return error / data.length;
+}
+
+export const trainData = (data: Data, epochs: number, learningRate = 0.01) => {
+  let slope = 0;
+  let intercept = 0;
+  for (let i = 0; i < epochs; i++) {
+    let slopeGradient = 0;
+    let interceptGradient = 0;
+
+    for (let j = 0; j < data.length; j++) {
+      const x = data[j].x;
+      const actualValue = data[j].actualX;
+      const pred = applyLinearRegression(slope, x, intercept);
+      const error = pred - actualValue;
+      slopeGradient += error * x;
+      interceptGradient += error;
+    }
+
+    slopeGradient = (2 / data.length) * slopeGradient;
+    interceptGradient = (2 / data.length) * interceptGradient;
+
+    slope -= learningRate * slopeGradient;
+    intercept -= learningRate * interceptGradient;
+
+ 
+      console.log(`Epoch ${epochs}, MSE: ${computeMSE(data, slope, intercept)}, ${data[0].x}`);
+      console.log({slope, intercept});
+
+  }
+};
