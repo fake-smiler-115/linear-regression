9b92ca3d206c3b04f7753728c55e8c968a36fa49
Gopi Vinay <gopivinay@Gopis-MacBook-Pro.local>
2026-05-08 22:33:16 +0530

    changes the directory structure.

--- linear-regression/src/App.tsx
+++ linear-regression/src/App.tsx
@@ -7,16 +7,16 @@ import { reducer } from "./reducer.ts";
 const App = () => {
   // const correctData = {slope : 15, intercept : 6};
   // const data: Data = [{x : 2, finalValue : 36}, {x : 1 , finalValue:21}, {x : 3 ,finalValue:51},{x : -10 ,finalValue:-144},{x : 0 ,finalValue:6}];
-  const correctData = {slope : 1, intercept : 1};
-  const data: Data = [{x : 1, finalValue : 2}];
-  // const correctData = { slope: 100, intercept: 34 };
+  // const correctData = {slope : 1, intercept : 1};
+  // const data: Data = [{x : 1, finalValue : 2}];
 
-  // const data: Data = [
-  //   { x: 1, finalValue: 134 },
-  //   { x: 0, finalValue: 34 },
-  //   { x: -1, finalValue: -66 },
-  //   { x: 10, finalValue: 1034 },
-  // ];
+  const correctData = { slope: 100, intercept: 34 };
+  const data: Data = [
+    { x: 1, finalValue: 134 },
+    { x: 0, finalValue: 34 },
+    { x: -1, finalValue: -66 },
+    { x: 10, finalValue: 1034 },
+  ];
   const [trainigDetails, dispatch] = useReducer(reducer, {
     slope: 0,
     intercept: 0,
@@ -24,7 +24,7 @@ const App = () => {
   });
 
   useEffect(() => {
-    const predictedValues = trainData(data, 100);
+    const predictedValues = trainData(data, 1000);
     for (let index = 0; index < predictedValues?.length; index++) {
       setTimeout(
         () =>
